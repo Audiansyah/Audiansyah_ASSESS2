3eb3d5de7ea3ea471c884c894f527e009c6b2899
Audiansyah <msaudiansyah@gmail.com>
2023-05-19 11:53:39 +0700

    commit 3

--- script.js
+++ script.js
@@ -37,4 +37,40 @@ $(document).ready(function() {
   
   // Menambahkan form ke dalam dokumen HTML
   document.body.appendChild(form);
+
+// Menambahkan event listener untuk submit form
+form.addEventListener('submit', function(event) {
+  // Mencegah pengiriman form secara default
+  event.preventDefault();
+
+  // Mendapatkan referensi ke input fields
+  const idInput = document.querySelector('input[name="id"]');
+  const adviceInput = document.querySelector('input[name="advice"]');
+  const dateInput = document.querySelector('input[name="date"]');
+
+  // Mendapatkan nilai dari input fields
+  const idValue = idInput.value;
+  const adviceValue = adviceInput.value;
+  const dateValue = dateInput.value;
+
+  // Melakukan validasi
+  if (idValue === '') {
+    alert('ID tidak boleh kosong');
+    return;
+  }
+
+  if (adviceValue === '') {
+    alert('Advice tidak boleh kosong');
+    return;
+  }
+
+  if (dateValue === '') {
+    alert('Date tidak boleh kosong');
+    return;
+  }
+
+  // Jika semua validasi berhasil, kirim form
+  form.submit();
+});
+
   
